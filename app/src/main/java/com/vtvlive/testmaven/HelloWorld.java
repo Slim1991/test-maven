c164bd71f34d6b72693e2861433b718530319092
Tu NM <tunm@dol-tech.com>
2017-12-14 10:14:41 +0700

    Init

---
+++ app/src/main/java/com/vtvlive/testmaven/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.vtvlive.testmaven;
+
+/**
+ * Created by Tu NM on 12/14/2017.
+ */
+
+public class HelloWorld {
+    public static String getMessage(){
+        return "Hello world!";
+    }
+}
